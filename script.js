aef8e6a5126c3b65f8701fef31f8b79bd17a1c84
SuarezRoig <santiagosuarez2604@gmail.com>
2025-12-05 12:32:17 +0100

    finishing css

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('play').addEventListener('click',()=>{
+    let musicContainer = document.querySelector('.music-container')
+    if(musicContainer.contains('play'))
+        musicContainer.
+    musicContainer.classList.add('play')
+})
