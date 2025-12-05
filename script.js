eecb0152c01cef88ea208a7ef4602994dd2ee2b8
SuarezRoig <santiagosuarez2604@gmail.com>
2025-12-05 15:39:30 +0100

    Making play, next and prev btn work

--- script.js
+++ script.js
@@ -12,15 +12,74 @@ const cover = document.getElementById('cover')
 
 
 
-const songs = []
+const Album = [{
+    name:'You Know You Like It',
+    image: 'images/Angel.png',
+    song: 'music/AlunaGeorge - You Know You Like It (Tchami Remix).mp3'
+},{
+    name:'Purple Line',
+    image: 'images/Caballero.png',
+    song: 'music/Ben Böhmer - Purple Line.mp3'
+},{
+    name:'Rock The Disco',
+    image: 'images/Jesus.png',
+    song: 'music/Chiqito - Rock The Disco (Original Mix).mp3'
+}
+]
+
+let currentSong = 0
 
 
 
+loadMusic()
+
+function loadMusic(){
+    audio.src = Album[currentSong].song
+    cover.src = Album[currentSong].image
+    title.innerText = Album[currentSong].name
+}
+
+
+nextButt.addEventListener('click',()=>{
+    currentSong = currentSong == Album.length-1 ? 0: currentSong+1
+    loadMusic()
+    if(!musicContainer.classList.contains('play'))
+        playButt.click()
+    audio.play()
+})
+
+prevButt.addEventListener('click',()=>{
+    currentSong = currentSong == 0 ? Album.length-1: currentSong-1
+    loadMusic()
+    if(!musicContainer.classList.contains('play'))
+        playButt.click()
+    audio.play()
+})
+
+function playMusic(){
+    musicContainer.classList.add('play')
+    playButt.querySelector('.fas').classList.remove('fa-play')
+    playButt.querySelector('.fas').classList.add('fa-pause')
+
+    audio.play()
+}
+
+function pauseMusic(){
+    musicContainer.classList.remove('play')
+    playButt.querySelector('.fas').classList.remove('fa-pause')
+    playButt.querySelector('.fas').classList.add('fa-play')
+    
+    audio.pause()
+}
 
 playButt.addEventListener('click',()=>{
-    if(musicContainer.classList.contains('play'))
-        musicContainer.classList.remove('play')
-    else musicContainer.classList.add('play')
+    let isPlaying = musicContainer.classList.contains('play') 
+
+    if(isPlaying)
+        pauseMusic()
+    else 
+        playMusic()
+    
 })
 
 
