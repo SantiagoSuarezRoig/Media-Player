b4a01750d08d34f09ed98ee88e731a94ec00b581
SuarezRoig <santiagosuarez2604@gmail.com>
2025-12-05 14:24:41 +0100

    adding all the DOM elements

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 
+const musicContainer = document.querySelector('.music-container')
+
+const playButt = document.getElementById('play') 
+const prevButt = document.getElementById('prev')
+const nextButt = document.getElementById('next')
+const audio = document.getElementById('audio')
+const title = document.getElementById('title')
+const progressContainer = document.querySelector('.progress-container')
+const progress = document.querySelector('progress')
+const cover = document.getElementById('cover')
+
+
+
+const songs = []
 
 
 
 
-document.getElementById('play').addEventListener('click',()=>{
-    let musicContainer = document.querySelector('.music-container')
+playButt.addEventListener('click',()=>{
     if(musicContainer.classList.contains('play'))
         musicContainer.classList.remove('play')
     else musicContainer.classList.add('play')
